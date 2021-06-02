aede27374674e08d4d210fec6b714d0a75fffa82
MblWa <you.are.empty@gmail.com>
2021-06-02 15:31:24 +0300

    Adds main page and card components, amount of cards is defined in index.js

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Main from '../main/main';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({ cardsCount }) {
+  return <Main cardsCount={cardsCount}/>;
 }
+
+App.propTypes = {
+  cardsCount: PropTypes.number.isRequired,
+};
 
 export default App;
