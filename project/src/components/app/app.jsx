7e88b723e967ba632372ba0efd6ad4d5ee02e9d0
MblWa <MblWa@users.noreply.github.com>
2021-06-03 23:36:28 +0300

    Adds basic routing via react router and  new components for corresponding pages

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Main from '../main/main';
+import SignIn from '../sign-in/sign-in';
+import Room from '../room/room';
+import Favorites from '../favorites/favorites';
+import NotFound from '../not-found/not-found';
+import { AppRoute } from '../../const';
 
 function App({ cardsCount }) {
-  return <Main cardsCount={cardsCount}/>;
+  const { ROOT, ROOM, LOGIN, FAVORITES } = AppRoute;
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={ROOT}>
+          <Main cardsCount={cardsCount}/>
+        </Route>
+        <Route exact path={ROOM} component={Room} />
+        <Route exact path={LOGIN}>
+          <SignIn />
+        </Route>
+        <Route exact path={FAVORITES}>
+          <Favorites />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
