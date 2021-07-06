ed15b1539b8d9ae1a2663dcdff3304d0fb1b186d
MblWa <MblWa@users.noreply.github.com>
2021-07-06 21:58:26 +0300

    Adds first test for api-actions. WIP

---
+++ project/src/store/api-actions.test.js
@@ -0,0 +1,32 @@
+import MockAdapter from 'axios-mock-adapter';
+import { createAPI } from '../services/api';
+import { ActionType } from './action';
+import { checkAuth } from './api-actions';
+import { APIRoute, AuthorizationStatus } from '../const';
+
+let api = null;
+
+describe('Async operations', () => {
+  beforeAll(() => {
+    api = createAPI(() => {});
+  });
+
+  it('should make a correct API call to GET /login', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const checkAuthLoader = checkAuth();
+
+    apiMock
+      .onGet(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return checkAuthLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+});
